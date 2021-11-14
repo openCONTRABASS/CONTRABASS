139902de201c9b73b7f17fd10a4b0892b7f3455d
alexOarga <alex718123@gmail.com>
2021-11-14 22:30:17 +0100

    fix test requirements, add coverage

--- test.sh
+++ test.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-py.test --log-cli-level=10 -s test/test_core/test_CobraMetabolicModel.py
-py.test --log-cli-level=10 -s test/test_cli/test_contrabass_cli.py 
+py.test --cov=./ --cov-report=xml --log-cli-level=10 -s test/test_core/test_CobraMetabolicModel.py
+py.test --cov=./ --cov-report=xml --log-cli-level=10 -s test/test_cli/test_contrabass_cli.py 
 
